20e40ae3c3d15200114efb6dcac068fc643a6af0
Stuart Miller <Stuart.j.miller.8182@gmail.com>
2019-07-09 21:41:34 -0500

    Add test for scraper.py

---
+++ tomatopy/tests/test_scraper.py
@@ -0,0 +1,36 @@
+import unittest
+
+from tomatopy.scraper import scrape_movie_info
+
+class TestScraper(unittest.TestCase):
+    def test_scrape_movie_info(self):
+        # test the main scraper
+        main_info, reviews = scrape_movie_info('x2: X-men united')
+
+        # verify keys exist
+        keys_main_info = main_info.keys()
+        self.assertTrue('synopsis' in keys_main_info)
+        self.assertTrue('rating' in keys_main_info)
+        self.assertTrue('genre' in keys_main_info)
+        self.assertTrue('director' in keys_main_info)
+        self.assertTrue('writer' in keys_main_info)
+        self.assertTrue('theater_date' in keys_main_info)
+        self.assertTrue('dvd_date' in keys_main_info)
+        self.assertTrue('currency' in keys_main_info)
+        self.assertTrue('box_office' in keys_main_info)
+        self.assertTrue('runtime' in keys_main_info)
+        self.assertTrue('studio' in keys_main_info)
+
+        # verify content of main_info dict
+        self.assertEqual(main_info['synopsis'][:30],'When a failed assassination at')
+        self.assertEqual(main_info['rating'],'PG-13')
+        self.assertEqual(main_info['genre'],'Action and Adventure|Science Fiction and Fantasy')
+        self.assertEqual(main_info['director'],'Bryan Singer')
+        self.assertEqual(main_info['writer'],'Daniel Harris|Dan Harris|Michael Dougherty|Bryan Singer|David Hayter')
+        self.assertEqual(main_info['theater_date'],'May 2, 2003')
+        self.assertEqual(main_info['dvd_date'],'Nov 25, 2003')
+        self.assertEqual(main_info['currency'],'$')
+        self.assertEqual(main_info['box_office'],'214,813,155')
+        self.assertEqual(main_info['runtime'],'134 minutes')
+        self.assertEqual(main_info['studio'],'20th Century Fox')
+    
